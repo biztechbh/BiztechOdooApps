4813feacd614fac7f47c9883243b5de359e56536
biztechbh <sale@biztech.bh>
2023-12-16 14:37:18 +0530

    [ADD] enterprise theme

---
+++ biztech_enterprise_theme/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': 'Enterprise Theme',
+    'version': '15.0.1.0.1',
+    'summary': 'Enterprise Theme',
+    'license': 'AGPL-3',
+    'author': 'Biztech Computer',
+    'maintainer': 'Biztech Computer',
+    'website': 'https://biztechbh.biz',
+    'depends': [
+        'web'
+    ],
+    'category': 'Branding',
+    'description': """
+           Odoo Enterprise Theme
+    """,
+    'assets': {
+        'web._assets_primary_variables': [
+            '/biztech_enterprise_theme/static/src/scss/primary_variables_custom.scss',
+        ],
+        'web.assets_common': [
+            '/biztech_enterprise_theme/static/src/scss/fields_extra_custom.scss',
+        ],
+        'web._assets_secondary_variables': [
+            '/biztech_enterprise_theme/static/src/scss/secondary_variables.scss',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': ['static/description/icon.png'],
+    
+}

7122abb15449229266a5f782185fb4025870cd77
biztechbh <sale@biztech.bh>
2023-12-16 15:49:18 +0530

    [ADD] vendor statement report

---
+++ customer_supplier_payment_statement/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Customer Supplier Payment Statement',
+    'version': '15.0',
+    'summary': "Customer Supplier Payment+ Statement Reports",
+    'sequence': 16,
+    'description': """
+                    Odoo Account Reports
+                    """,
+    'category': 'purchase',
+    'author': 'Biztech Computer',
+    'maintainer': 'Biztech Computer',
+    'website': 'https://biztechbh.biz',
+    'depends': ['account', 'purchase', 'report_xlsx'],
+    'data': [
+        'security/ir.model.access.csv',
+        'reports/report.xml',
+        'views/res_partner_views.xml',
+        'reports/report_template.xml',
+        'wizard/customer_statement_wizard.xml',
+        'reports/vendor_report_template.xml',
+        'data/ir_cron_data.xml'
+    ],
+    'assets': {
+        'web.assets_backend': [
+            '/customer_vendor_statement/static/src/js/action_manager.js',
+        ]
+    },
+    'demo': [],
+    'license': 'AGPL-3',
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}

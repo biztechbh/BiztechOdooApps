24c97737242f04c2145125e794a71acf30ae8e7f
Arun <biztechbh>
2023-12-30 18:54:51 +0530

    [ADD] pos hourly sale report

---
+++ biztech_hourly_sales_report/__manifest__.py
@@ -0,0 +1,27 @@
+
+{
+    'name': 'POS Hourly Sales Report',
+    'version': '14.0',
+    'summary': "POS Hourly Sales Reports",
+    'sequence': 16,
+    'description': """  POS Hourly Sales Reports""",
+    'category': 'sales',
+    'author': 'Biztech Computer',
+    'maintainer': 'Biztech Computer',
+    'website': 'https://biztechbh.biz',
+    "depends": ['point_of_sale'],
+    "data": [
+        'views/report_view.xml',
+        'wizard/dashboard_view.xml',
+        'wizard/wizard_report.xml',
+    ],
+    'qweb': [
+        'static/src/xml/sale_view.xml',
+    ],
+    'license': 'AGPL-3',
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+

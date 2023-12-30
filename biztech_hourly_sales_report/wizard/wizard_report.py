db42fa4b97e1332f9ba1d77e92bc08283331600a
Arun <biztechbh>
2023-12-30 18:11:14 +0530

    [ADD] pos hourly sale report

---
+++ biztech_hourly_sales_report/wizard/wizard_report.py
@@ -0,0 +1,33 @@
+
+from odoo import models, fields, api
+from collections import OrderedDict
+import ast
+from ast import literal_eval
+import json
+import datetime
+
+
+class ReportWizardDownload(models.TransientModel):
+    _name = 'report.hourly.sale.wizard.download'
+
+    name = fields.Char(string='File Name', readonly=True)
+    data = fields.Binary(string='File', readonly=True)
+    data_pdf = fields.Text(string="Data")
+    summary = fields.Text(string="Summary")
+
+    def get_data(self):
+        if self.data_pdf:
+            dict = eval(self.data_pdf)
+            return dict
+
+    def get_sorted_data(self, data_old, is_per_date):
+        if data_old and not is_per_date:
+            return OrderedDict(sorted(data_old.items(), key=lambda t: int(t[0])))
+        elif data_old and is_per_date:
+            return OrderedDict(sorted(data_old.items(), key=lambda t: t[0]))
+
+    def get_summary(self):
+        if self.summary:
+            dict = eval(self.summary)
+            return dict
+

bd4c475460d08574c79858f4db2d4ccc73e329b1
Arun <biztechbh>
2023-12-31 10:21:31 +0530

    [ADD] pos daily sale report

---
+++ biztech_daily_sales_report/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'POS Daily Sales Report',
+    'version': '12.0',
+    'summary': "POS Daily Sales Report",
+    'sequence': 16,
+    'description': """  POS Daily Sales Report""",
+    'category': 'sales',
+    'author': 'Biztech Computer',
+    'maintainer': 'Biztech Computer',
+    'website': 'https://biztechbh.biz',
+    "depends": ['point_of_sale'],
+    "data": [
+        'views/report_view.xml',
+        'wizard/dashboard_view.xml',
+        'wizard/wizard_report.xml',
+    ],
+    'qweb': [
+        'static/src/xml/sale_view.xml',
+    ],
+    'license': 'AGPL-3',
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ biztech_daily_sales_report/wizard/wizard_report.py
@@ -0,0 +1,36 @@
+
+from odoo import models, fields, api
+from collections import OrderedDict
+import ast
+from ast import literal_eval
+import json
+import datetime
+
+
+class ReportWizardDownload(models.TransientModel):
+    _name = 'report.daily.sale.wizard.download'
+
+    name = fields.Char(string='File Name', readonly=True)
+    data = fields.Binary(string='File', readonly=True)
+    data_pdf = fields.Text(string="Data")
+    summary = fields.Text(string="Summary")
+
+    @api.multi
+    def get_data(self):
+        if self.data_pdf:
+            dict = eval(self.data_pdf)
+            return dict
+
+    @api.multi
+    def get_sorted_data(self, data_old, is_per_date):
+        if data_old and not is_per_date:
+            return OrderedDict(sorted(data_old.items(), key=lambda t: int(t[0])))
+        elif data_old and is_per_date:
+            return OrderedDict(sorted(data_old.items(), key=lambda t: t[0]))
+
+    @api.multi
+    def get_summary(self):
+        if self.summary:
+            dict = eval(self.summary)
+            return dict
+
